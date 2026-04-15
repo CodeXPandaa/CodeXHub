8b53b2140c168016244c1ce4356442e45cba7925
Abhishek Gautam <166837659+CodeXPandaa@users.noreply.github.com>
2026-04-15 12:31:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
     ? process.env.FRONTEND_URL
-    : ['https://code-x-hub.vercel.app/login', 'http://localhost:3001'],
+    : ['https://code-x-hub.vercel.app', 'http://localhost:3001'],
   credentials: true,
 }));
 app.use(express.json());
